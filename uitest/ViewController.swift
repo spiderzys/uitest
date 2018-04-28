68260724cbae9ce2039634b135d4938ee87a0f99
spiderzys <yzou@yangshengs-macbook-pro.local>
2018-04-28 16:44:07 -0400

    Initial Commit

---
+++ uitest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uitest
+//
+//  Created by Yangsheng Zou on 2018-04-28.
+//  Copyright © 2018 Yangsheng Zou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
